917fa31f8b88894bea88715c9922cc0380a4b5b1
narf200 <narf200@gmail.com>
2023-04-17 17:49:26 +0200

    feat: add binding data between paren and child, between child and parent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'data-binding';
+  serverElements = [{type:"server", name:'Test server', content:'hahha'}];
+  listOfEvenCounters: number[] = [];
+  listOfOddCounters: number[] = [];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+    console.log(this.serverElements)
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+    console.log(this.serverElements)
+  }
+
+  isEven( count: number ) {
+    console.log('isEven', count % 2 === 0)
+    return count % 2 === 0
+  }
+
+  onCounterIncreased(count: number ) {
+    console.log('count ==>>', count)
+    console.log('isEven', count % 2 === 0)
+    console.log('listOfOddCounters', this.listOfOddCounters)
+    console.log('listOfEvenCounters', this.listOfEvenCounters)
+    if(this.isEven(count)) {
+      this.listOfEvenCounters.push(count)
+    } else {
+      this.listOfOddCounters.push(count)
+    }
+  }
+
+  onDelete() {
+    console.log(this.serverElements.length - 1)
+    this.serverElements.splice(this.serverElements.length - 1,1)
+  }
+
+
+
 }
